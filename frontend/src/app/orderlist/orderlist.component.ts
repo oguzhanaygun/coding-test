02e60af696bc6ee249695f723f968e8b40254ffc
oguzhanaygun <o.aygun34@gmail.com>
2022-10-14 16:50:33 +0300

    order table added to ui

--- frontend/src/app/orderlist/orderlist.component.ts
+++ frontend/src/app/orderlist/orderlist.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../service/order.service';
+import { Order } from '../model/order';
+import { Customer } from '../model/customer';
+import { CustomerService } from '../service/customer.service';
 
 @Component({
   selector: 'app-orderlist',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orderlist.component.css']
 })
 export class OrderlistComponent implements OnInit {
-
-  constructor() { }
+  orders : Array<Order> = [];
+  customers : any;
+  loading: boolean = true;
+  hasErr:boolean = false;
+  constructor(private orderService: OrderService, private customerService:CustomerService) { }
 
   ngOnInit(): void {
+    this.getOrders();
   }
 
+  private getOrders() {
+    this.orderService.getOrders().subscribe({
+      next: (orders) => {
+        this.orders = orders;
+      },
+      error: (error) => {
+        console.log(error);
+        this.hasErr = true;
+      },
+      complete: () => {
+        console.log('Request complete');
+        this.loading = false;
+      }
+    });
+  }
+
+   getCustomersName(id:number) {
+    return this.customerService.getCustomer(id).name;
+  }
 }
