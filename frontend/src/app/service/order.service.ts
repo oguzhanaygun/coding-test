e016d6bcbd312c873c552a274007a23c7f3a0cec
oguzhanaygun <o.aygun34@gmail.com>
2022-10-17 01:24:37 +0300

    order service mock save function

--- frontend/src/app/service/order.service.ts
+++ frontend/src/app/service/order.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Order } from '../model/order';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
   private dataUrl = 'assets/data/orders.json';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  getOrders():Observable<Order[]>{
+  getOrders(): Observable<Order[]> {
     return this.http.get<Order[]>(this.dataUrl);
   }
+
+  saveOrder(order:Order):Observable<any>{
+    //return this.http.post<Order>(this.dataUrl,order);
+    return of({});
+  }
+
 }
